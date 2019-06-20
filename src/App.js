c8497ea181d4ccf1f6aad656de2cf5eebe035343
Toua Thao <touathao09@gmail.com>
2019-06-20 18:49:13 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PokeCard from './PokeCard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Apps</h1>
+      <PokeCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PokeCard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class PokeCard extends Component{
+    render(){
+        return(
+            <h1>PokeCard!!</h1>
+        )
+    }
+}
+
+export default PokeCard;
